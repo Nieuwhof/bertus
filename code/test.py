5c53476d063ae76b92ad814c20bc1fbeaefd085b
Lambertus Nieuwhof <bnieuwhof@gmail.com>
2022-12-28 05:06:42 -0800

    player addeed

--- code/test.py
+++ code/test.py
@@ -4,18 +4,26 @@ import random
 items = ["Spider","Troll","Vampire","Wolf","Thing", "leech", "Bombardier Bug", "Zombie"]
 items2 = [100,80,85,70,65,71,45,90,55,43,23,11,5]
 
-class Monster:
-    def __init__(self, name, life):
+class player:
+    def __init__(self):
+        life = 100
+        name = 'Player'
         self.name = name
         self.life = life
-        if name == '':
-            self.name = random.choice(items)
-            self.life = random.choice(items2)
 
-mon1 = Monster('test',10)
-mon2 = Monster('',0) 
-mon3 = Monster('',0)  
+
+class Monster:
+    def __init__(self):
+        name = random.choice(items)
+        life = random.choice(items2)
+        self.name = name
+        self.life = life
+
+mon1 = Monster()
+play = player()
+mon3 = Monster()  
 
 print(mon1.name, mon1.life)
-print(mon2.name, mon2.life)
+print(play.name, play.life)
 print(mon3.name, mon3.life)
+
